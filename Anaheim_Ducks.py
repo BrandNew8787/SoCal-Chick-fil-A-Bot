5762948a113f79d98ffbaf89cba5135c674d5226
BrandNew8787 <80979158+BrandNew8787@users.noreply.github.com>
2024-09-09 15:41:16 -0700

    Update Anaheim_Ducks.py
    Optimized code and formatting

--- Anaheim_Ducks.py
+++ Anaheim_Ducks.py
@@ -1,7 +1,6 @@
-import asyncio
-import aiohttp  # For asynchronous HTTP requests
-import requests
 from datetime import datetime
+
+import aiohttp  # For asynchronous HTTP requests
 
 today = datetime.today().strftime('%Y-%m-%d')
 # NHL_API_URL: [str] = f"https://api-web.nhle.com/v1/score/{today}"
@@ -10,19 +9,20 @@ NHL_API_URL: [str] = f"https://api-web.nhle.com/v1/score/2024-04-18"
 
 async def get_ducks_next_home_game():
     # Define the endpoint for the Ducks' schedule
-    team_id = 24  # Anaheim Ducks' team ID in the NHL API
     schedule_url = f"https://api-web.nhle.com/v1/club-schedule-season/ANA/now"
-    today = datetime.today().strftime('%Y-%m-%d')
+    today_home = datetime.today().strftime('%Y-%m-%d')
 
     async with aiohttp.ClientSession() as session:
         async with session.get(schedule_url) as response:
             schedule_data = await response.json()
 
     for row in schedule_data['games']:
-        if row['homeTeam']['id'] == 24 and row['gameDate'] >= today:
-            return f"Next Ducks Home Game: \n\tDate: {row['gameDate']}\n\tOpp: {row['awayTeam']['placeName']['default']}"
+        if row['homeTeam']['id'] == 24 and row['gameDate'] >= today_home:
+            return row['gameDate'], row['awayTeam']['placeName']['default']
 
     return "No upcoming home games found."
+
+
 # gameOutcome
 
 async def ducks_home_game_today():
@@ -45,7 +45,6 @@ async def check_ducks_score():
 
     # Check if it was a Ducks home game and they scored 5 points
     daily_games = data_nhl['games']
-    free_chick_fil_a = False
     for i in daily_games:
         if i['homeTeam']['id'] == 24:
             if 'scores' in i['homeTeam'] and 'gameOutcome' in i:
@@ -57,9 +56,15 @@ async def check_ducks_score():
 
 
 # async def main():
-#     result = await check_ducks_score()
-#     if result:
-#         print(result)
+#     result = await get_ducks_next_home_game()
+#     print(result)
+#     # Uncomment if you want to run the check_ducks_score function
+#     # score_result = await check_ducks_score()
+#     # if score_result:
+#     #     print("Ducks scored 5 points!")
+#     # else:
+#     #     print("The game hasn't finished, or they haven't scored 5 points yet.")
+#
 #
 # # Run the async function
 # if __name__ == "__main__":
